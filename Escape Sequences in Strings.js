7bebd172705be62256a0d973954bb62ea27fc0cd
Niernines <106055885+Niernines@users.noreply.github.com>
2022-06-02 13:17:35 +0200

    Initial commit

---
+++ Escape Sequences in Strings.js
@@ -0,0 +1,13 @@
+// We learned this in the previous challenge.
+
+//   Code  	Output
+//  \'	single quote
+//  \"	double quote
+//  \\	backslash
+//  \n	newline
+//  \r	carriage return
+//  \t	tab
+//  \b	word boundary
+// \f	form feed
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";// Change this line
---
+++ Plus double.js
@@ -0,0 +1,9 @@
+let myVar = 11;
+myVar--
+// Only change code below this line
+myVar = myVar - 0;
+
+let youVar = 11;
+youVar++
+// Only change code below this line
+youVar = youVar + 0;
